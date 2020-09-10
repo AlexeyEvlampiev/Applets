69355d75893b7a32ccd29d79ff9d1d1d1f5069a5
Alexey Evlampiev <alexey.evlampiev@gmail.com>
2020-09-10 16:15:45 +0200

    wip

---
+++ src/Applets.XUnitTest/Extensions_ToObservable_Should.cs
@@ -0,0 +1,20 @@
+﻿using System.Reactive.Threading.Tasks;
+using System.Threading;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Applets
+{
+    public class Extensions_ToObservable_Should
+    {
+        [Fact]
+        public void CreateFromCancellationToken()
+        {
+            var target = new CancellationTokenSource();
+            var task = target.Token.ToObservable().ToTask(CancellationToken.None);
+            Assert.False(task.IsCompleted);
+            target.Cancel();
+            Assert.True(task.IsCompleted);
+        }
+    }
+}
